ced2967c81630890404d25d0aedc4ac039b4f99f
Sascha Dörnath <sascha.doernath@gmx.de>
2020-02-03 06:23:44 +0100

    Added picture component and call to face recognition API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
+
+const app = new Clarifai.App({
+ apiKey: '6b34c831456a44ecab0b2fb5cc883140'
+});
 
 const particlesOptions = {
   particles: {
@@ -20,6 +25,36 @@ const particlesOptions = {
 }
 
  class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: "",
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    console.log("click");
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        // URL
+        this.state.input,
+      )
+      .then(function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
@@ -29,8 +64,11 @@ const particlesOptions = {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/*<FaceRecognition />*/}
+        <ImageLinkForm 
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Tilt from 'react-tilt';
 import pic from './Logo.png'
 import './Logo.css';
